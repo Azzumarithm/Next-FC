b23a701bf67c139ae4d8060e2ec021cf1e077f3e
azzumar247 <azzumar.tbskh@gmail.com>
2024-05-18 03:42:13 +0800

    first commit

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv"
+import { defineConfig } from 'drizzle-kit';
+
+dotenv.config({
+  path: ".env.local"
+})
+
+export default defineConfig({
+  schema: './app/db/schema.ts',
+  out: './migrations',
+  dialect: 'sqlite',
+  driver: 'turso',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_TOKEN!,
+  },
+});
